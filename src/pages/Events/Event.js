24e06c5aee5edafd59d07e78bab8263b14e67719
Andrew Daniel <andrewjd1@gmail.com>
2018-02-24 22:00:16 +0000

    allow selecting of seats

--- src/pages/Events/Event.js
+++ src/pages/Events/Event.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import times from 'lodash/times';
 import PropTypes from 'prop-types';
 
+import './Event.css';
+
 // cheap way
 const rowLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 class Event extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            seatsChosen: []
+        }
+    }
+
     handleSeatBooked = (seatId) => {
-        console.log(seatId, 'clicked');
+        this.setState({
+            seatsChosen: [...this.state.seatsChosen, seatId]
+        });
     }
 
     render() {
         const { event } = this.props;
+        const { seatsChosen } = this.state;
 
         const rows = 10;
         const cols = 10;
@@ -25,21 +37,40 @@ class Event extends Component {
 
                 <h2>Book a seat</h2>
 
-                { times(rows, rowNum => {
-                    const rowLetter = rowLetters[rowNum];
-                    const row = times(cols, colNum => {
-                        const col = colNum + 1;
-                        const seatId = `${rowLetter}${col}`;
-                        // is this efficient? depends on the API and the Reducer State I guess...
-                        const isBooked = event.seatsBooked.some(booking => booking.seat === seatId);
-                        return (
-                        <button key={seatId}onClick={() => this.handleSeatBooked(seatId)} disabled={isBooked}>
-                            {seatId} {isBooked && 'BOOKED'}
-                        </button>
-                        )
-                    })
-                    return <div key={rowLetter}>{row}</div>;
-                }) }
+                <div className="booking">
+                <div className="booking--seats">
+                    { times(rows, rowNum => {
+                        const rowLetter = rowLetters[rowNum];
+                        const row = times(cols, colNum => {
+                            const col = colNum + 1;
+                            const seatId = `${rowLetter}${col}`;
+                            // is this efficient? depends on the API and the Reducer State I guess...
+                            const isBooked = event.seatsBooked.some(booking => booking.seat === seatId);
+                            return (
+                            <button 
+                                className="seat"
+                                key={seatId}
+                                onClick={() => this.handleSeatBooked(seatId)}
+                                disabled={isBooked} 
+                                title={isBooked ? 'Booked' : 'Available'}>
+                                {seatId}
+                            </button>
+                            )
+                        })
+                        return <div key={rowLetter}>{row}</div>;
+                    }) }
+                </div>
+                { !!seatsChosen.length && (
+                    <aside className="booking--info">
+                        <p>You have chosen {seatsChosen.length} seats</p>
+                        <ul>
+                            {seatsChosen.map(seat => (
+                                <li>{seat}</li>
+                            ))}
+                        </ul>
+                    </aside>
+                )}
+                </div>
 
             </section>
         )
